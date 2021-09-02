c09405d4f8019bb71a8e1651863fe41795d91906
zengqi <136800645@qq.com>
2021-09-02 15:35:56 +0800

    tet

---
+++ app.js
@@ -0,0 +1,8 @@
+const Koa = require("koa");
+const staticServe = require("koa-static");
+const app = new Koa();
+
+app.use(staticServe(__dirname));
+app.listen(9527,() => {
+  console.log("Server started at 9527.");
+});
